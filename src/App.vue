f87f4f0da534e8d17e17ce48ebc923ddde206944
Andrew Bavyka <abavyka@list.ru>
2023-06-26 00:01:22 +0500

    начало положено

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TitleApp from './components/TitleApp.vue'
+import CurrencyInput from './components/CurrencyInput.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+    <TitleApp />
+    <CurrencyInput />
+  </div>
+</template>
+
+<style>
+.wrapper {
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 20px 15px 20px 15px;
+  background-color: #F6F6F6;
+  border-radius: 14px;
+}
+</style>
---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+const numbers = ref();
+
+</script>
+
+<template>
+    <input v-model="numbers" class="input-currency" type="number">
+    <p>Сообщение {{ numbers }}</p>
+</template>
+    
+<style scoped>
+p {
+    color: red;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+    -moz-appearance: textfield;
+}
+
+.input-currency {
+    border: none;
+    max-width: 100%;
+    padding: 0 0 0 5px;
+    font-family: Roboto;
+    font-size: 20px;
+    color: #3C3C3C;
+    border-radius: 7px;
+    background: #EFEFEF;
+}
+</style>
